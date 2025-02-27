a729d62b669f1a4f81908146359ecb4bc02a6eec
nilusink <nilusink@protonmail.com>
2025-02-27 11:15:04 +0100

    comments

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import pandas as pd
 import sqlite3
 import os
 
+# selected applications
 APPLICATIONS: list[str] = [
     "pycharm",
 #    "firefox",
@@ -28,14 +29,16 @@ APPLICATIONS: list[str] = [
     "DesktopEditors"
 ]
 
+
 # database connection
 conn = sqlite3.connect(f"{os.getenv('HOME')}/.local/share/whatpulse.db")
 
 # Plot data
 plt.figure(figsize=(10, 5))
 
+# graph each application
 for application in APPLICATIONS:
-    # Connect to SQLite and load data into Pandas
+    # load data into Pandas
     query = f"SELECT day, hour, seconds_active FROM application_active_hour WHERE path LIKE '%{application}%';"  # " WHERE path like '%firefox%'"
     df = pd.read_sql_query(query, conn)
     conn.close()
@@ -43,9 +46,12 @@ for application in APPLICATIONS:
     # Convert date column to datetime
     df['day'] = pd.to_datetime(df['day'])
     df['datetime'] = pd.to_datetime(df['day'].astype(str) + ' ' + df['hour'].astype(str))
+
+    # convert column to cumulative hours
     df['hours_active'] = df['seconds_active'] / 3600
     df['total_hours'] = df['hours_active'].cumsum()
 
+    # plot application
     plt.plot(df['datetime'], df['total_hours'], linestyle='-', label=application.capitalize())
 
 # Labels and title
